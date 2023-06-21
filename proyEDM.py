9749cc03877002d78c47cbd4118df08162d6f3e9
albamrtnz <94964555+albamrtnz@users.noreply.github.com>
2023-06-21 19:01:53 +0200

    Update proyEDM.py

--- proyEDM.py
+++ proyEDM.py
@@ -14,15 +14,57 @@ import time
 from geopy.geocoders import Nominatim
 from time import sleep
 loc = Nominatim(user_agent="GetLoc")
-
+st.title("Nearest Bike Parking Finder")
 st.set_page_config(page_title="ParkingFinder", layout='wide', initial_sidebar_state = "auto")
 data = {'lat': [], 'lon': []}
 
+import requests
+
+def obtener_tiempo_madrid():
+    url = "http://api.weatherapi.com/v1/current.json"
+    params = {
+        "key": "ebbc696950b4436a88a155849232106",
+        "q": "Madrid"
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data
+
+# Obtener los datos del tiempo actual de Madrid
+tiempo_madrid = obtener_tiempo_madrid()
+
+
+from datetime import datetime, timedelta
+
+def obtener_tiempo_madrid_anterior(fecha):
+    url = "http://api.weatherapi.com/v1/history.json"
+    params = {
+        "key": "ebbc696950b4436a88a155849232106",
+        "q": "Madrid",
+        "dt": fecha.strftime("%Y-%m-%d"),
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data
+
+# Calcular la fecha del día anterior
+fecha_anterior = datetime.now() - timedelta(days=1)
+
+# Obtener los datos del tiempo del día anterior en Madrid
+tiempo_anterior = obtener_tiempo_madrid_anterior(fecha_anterior)
+
+st.title("Weather")
+   
+m1,m2,m3 = st.columns(3)
+m1.metric(label="Temperature", value= "{}°C".format(tiempo_madrid['current']['temp_c']), delta = "{}°C".format(round(tiempo_madrid['current']['temp_c'] - tiempo_anterior['forecast']['forecastday'][0]['day']['avgtemp_c'],1)))
+
+m2.metric(label="Humidity", value= "{}%".format(tiempo_madrid['current']['humidity']), delta = "{}%".format(round(tiempo_madrid['current']['humidity'] - tiempo_anterior['forecast']['forecastday'][0]['day']['avghumidity'],1)))
+
+m3.metric(label= "Conditions", value="{}".format(tiempo_madrid['current']['condition']['text']), delta = "{}".format(tiempo_anterior['forecast']['forecastday'][0]['day']['condition']['text']), delta_color= 'off')
 
 coord_geo = {"Madrid" : {"lon": -3.7025600, "lat": 40.4165000}}
 col1, col2, col3= st.columns(3)
 with col1:
-    st.title("Nearest Bike Parking Finder")
     ciudad = "Madrid"
     destino = st.text_input("Origin", "Ejemplo: Gran Vía")
 
@@ -79,51 +121,9 @@ with col1:
     # Mostrar el mapa interactivo en Streamlit
     st.plotly_chart(fig)
 
-with col3:
-    import requests
-
-    def obtener_tiempo_madrid():
-        url = "http://api.weatherapi.com/v1/current.json"
-        params = {
-            "key": "ebbc696950b4436a88a155849232106",
-            "q": "Madrid"
-        }
-        response = requests.get(url, params=params)
-        data = response.json()
-        return data
     
-    # Obtener los datos del tiempo actual de Madrid
-    tiempo_madrid = obtener_tiempo_madrid()
-
-
-    from datetime import datetime, timedelta
-
-    def obtener_tiempo_madrid_anterior(fecha):
-        url = "http://api.weatherapi.com/v1/history.json"
-        params = {
-            "key": "ebbc696950b4436a88a155849232106",
-            "q": "Madrid",
-            "dt": fecha.strftime("%Y-%m-%d"),
-        }
-        response = requests.get(url, params=params)
-        data = response.json()
-        return data
-    
-    # Calcular la fecha del día anterior
-    fecha_anterior = datetime.now() - timedelta(days=1)
-    
-    # Obtener los datos del tiempo del día anterior en Madrid
-    tiempo_anterior = obtener_tiempo_madrid_anterior(fecha_anterior)
 
     # Mostrar los datos en la página web
-    st.title("Weather")
-   
-m1,m2,m3 = st.columns(3)
-m1.metric(label="Temperature", value= "{}°C".format(tiempo_madrid['current']['temp_c']), delta = "{}°C".format(round(tiempo_madrid['current']['temp_c'] - tiempo_anterior['forecast']['forecastday'][0]['day']['avgtemp_c'],1)))
-
-m2.metric(label="Humidity", value= "{}%".format(tiempo_madrid['current']['humidity']), delta = "{}%".format(round(tiempo_madrid['current']['humidity'] - tiempo_anterior['forecast']['forecastday'][0]['day']['avghumidity'],1)))
-
-m3.metric(label= "Conditions", value="{}".format(tiempo_madrid['current']['condition']['text']), delta = "{}".format(tiempo_anterior['forecast']['forecastday'][0]['day']['condition']['text']), delta_color= 'off')
 
     
     
