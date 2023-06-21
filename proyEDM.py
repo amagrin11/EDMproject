c23de3d412fbc95d269628a5216f11d90b77b113
albamrtnz <94964555+albamrtnz@users.noreply.github.com>
2023-06-21 19:03:03 +0200

    Update proyEDM.py

--- proyEDM.py
+++ proyEDM.py
@@ -63,63 +63,63 @@ m2.metric(label="Humidity", value= "{}%".format(tiempo_madrid['current']['humidi
 m3.metric(label= "Conditions", value="{}".format(tiempo_madrid['current']['condition']['text']), delta = "{}".format(tiempo_anterior['forecast']['forecastday'][0]['day']['condition']['text']), delta_color= 'off')
 
 coord_geo = {"Madrid" : {"lon": -3.7025600, "lat": 40.4165000}}
-col1, col2, col3= st.columns(3)
-with col1:
-    ciudad = "Madrid"
-    destino = st.text_input("Origin", "Ejemplo: Gran Vía")
+
+
+ciudad = "Madrid"
+destino = st.text_input("Origin", "Ejemplo: Gran Vía")
 
 
 # Insertar un espacio en blanco para poner "elegir tema" abajo del todo
-    df_puntos = {'lat':[], 'lon':[]}
-    lista_bicis = pd.read_csv("bicis1.csv", header = 0, sep = ';', encoding='latin-1')
-    if destino != "Eje: Gran Vía":
-        def dirToCoord(df, calle):
-            minimo = 10000000
-            for i in range(len(df)):
-                eje_lon = abs(float(df.loc[i,'Longitud'])-calle.longitude)
-                eje_lat = abs(float(df.loc[i,'Latitud'])-calle.latitude)
-                a = eje_lat + eje_lon
-                if a < minimo:
-                    minimo = a
-                    fila = i
-            return fila
+df_puntos = {'lat':[], 'lon':[]}
+lista_bicis = pd.read_csv("bicis1.csv", header = 0, sep = ';', encoding='latin-1')
+if destino != "Eje: Gran Vía":
+    def dirToCoord(df, calle):
+        minimo = 10000000
+        for i in range(len(df)):
+            eje_lon = abs(float(df.loc[i,'Longitud'])-calle.longitude)
+            eje_lat = abs(float(df.loc[i,'Latitud'])-calle.latitude)
+            a = eje_lat + eje_lon
+            if a < minimo:
+                minimo = a
+                fila = i
+        return fila
+    
+    if "Madrid, España" not in destino:
+        u = destino + ", Madrid, España"
+        getLoc_calle = loc.geocode(destino)
+        if getLoc_calle:
+            row = dirToCoord(lista_bicis, getLoc_calle)
+            #a = lista_coords.loc[lista_coords.loc[:,0] == dirToCoord(lista_coords, getLoc_calle)]
+            df_puntos['lon'].append(float(lista_bicis.loc[row, 'Longitud']))
+            df_puntos['lat'].append(float(lista_bicis.loc[row, 'Latitud']))
+                                                                             
+        else:
+            pass
+    if len(df_puntos['lat']) > 0:
+        fig = go.Figure(go.Scattermapbox(
+            mode='markers+text',
+            name = 'PARKING',
+            lat=[df_puntos['lat'][0]],
+            lon=[df_puntos['lon'][0]],
+            marker = {'size': 9, 'color':'red'},
+                ))
+        centered = {'lon' : df_puntos['lon'][0],'lat' : df_puntos['lat'][0]}
+        fig.update_layout(mapbox_style="carto-positron", mapbox=dict(center = centered, zoom = 15))
+        fig.update_layout(height=900,width=1000) 
+        u = lista_bicis.loc[row, 'Calle'] +", Madrid, España"
+        st.write(loc.geocode(u))
+        st.write(lista_bicis.loc[row][9])
+        st.write(lista_bicis.loc[row, 'Distrito'])
         
-        if "Madrid, España" not in destino:
-            u = destino + ", Madrid, España"
-            getLoc_calle = loc.geocode(destino)
-            if getLoc_calle:
-                row = dirToCoord(lista_bicis, getLoc_calle)
-                #a = lista_coords.loc[lista_coords.loc[:,0] == dirToCoord(lista_coords, getLoc_calle)]
-                df_puntos['lon'].append(float(lista_bicis.loc[row, 'Longitud']))
-                df_puntos['lat'].append(float(lista_bicis.loc[row, 'Latitud']))
-                                                                                 
-            else:
-                pass
-        if len(df_puntos['lat']) > 0:
-            fig = go.Figure(go.Scattermapbox(
-                mode='markers+text',
-                name = 'PARKING',
-                lat=[df_puntos['lat'][0]],
-                lon=[df_puntos['lon'][0]],
-                marker = {'size': 9, 'color':'red'},
-                    ))
-            centered = {'lon' : df_puntos['lon'][0],'lat' : df_puntos['lat'][0]}
-            fig.update_layout(mapbox_style="carto-positron", mapbox=dict(center = centered, zoom = 15))
-            fig.update_layout(height=900,width=1000) 
-            u = lista_bicis.loc[row, 'Calle'] +", Madrid, España"
-            st.write(loc.geocode(u))
-            st.write(lista_bicis.loc[row][9])
-            st.write(lista_bicis.loc[row, 'Distrito'])
-            
-        else: 
-            fig = px.scatter_mapbox(df_puntos, lat='lat', lon='lon', center = coord_geo['Madrid'], zoom = 11)
-            fig.update_layout(mapbox_style="carto-positron")
-            fig.update_layout(height=900,width=1000)
-    
-    
-    
-    # Mostrar el mapa interactivo en Streamlit
-    st.plotly_chart(fig)
+    else: 
+        fig = px.scatter_mapbox(df_puntos, lat='lat', lon='lon', center = coord_geo['Madrid'], zoom = 11)
+        fig.update_layout(mapbox_style="carto-positron")
+        fig.update_layout(height=900,width=1000)
+
+
+
+# Mostrar el mapa interactivo en Streamlit
+st.plotly_chart(fig)
 
     
 
