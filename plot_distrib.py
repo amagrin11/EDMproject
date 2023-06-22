7c8d7f1f623724bf15cffb2ac6ff903e6a847a7f
albamrtnz <94964555+albamrtnz@users.noreply.github.com>
2023-06-22 13:32:39 +0200

    Update plot_distrib.py

--- plot_distrib.py
+++ plot_distrib.py
@@ -16,8 +16,9 @@ def distribution_plot(parking_x):
   # Agregar una línea vertical para marcar la media
   plt.axvline(x=mean, color='black', linestyle='--', label='Mean', alpha = 0.5)
   plt.axvline(x=parking_x, color='#86E0CE', linestyle='--')
+  
   plt.text(parkin_x, plt.ylim()[1]*0.8, f'{parkin_x}',ha='right', va='top',bbox = dict(facecolor='white', alpha=0.5))
-  plt.text(plt.xlim()[1]*0.99, plt.ylim()[1]*0.8, f'Mean = {mean:.2f}',ha='right', va='top',bbox = dict(facecolor='white', alpha=0.5))
+  #plt.text(plt.xlim()[1]*0.99, plt.ylim()[1]*0.8, f'Mean = {mean:.2f}',ha='right', va='top',bbox = dict(facecolor='white', alpha=0.5))
 
   # Configuraciones adicionales
   plt.xlabel("Available slots")
