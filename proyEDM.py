ae55576797d1ab35e2390966803a1ab1a05d7573
albamrtnz <94964555+albamrtnz@users.noreply.github.com>
2023-06-26 19:12:50 +0200

    Update proyEDM.py

--- proyEDM.py
+++ proyEDM.py
@@ -148,9 +148,9 @@ with col2:                                                #Column for the map
 
 with col1:                                                #Column for the extra information
     if destino != "Example: Gran Vía" and loc.geocode(u):
-        st.write(f'**__Origin:__** {loc.geocode(u)}')
+        st.write(f'**Origin:** {loc.geocode(u)}')
         u2 = f'{lista_bicis.loc[row, "Calle"]}, Madrid, España'
-        st.write(f'**__Bike parking:__** {loc.geocode(u2)}')
+        st.write(f'**Bike parking:** {loc.geocode(u2)}')
         st.write('--------')
         st.metric(label = "Capacity", value = f'{lista_bicis.iloc[row, 9]} bikes')
         st.metric(label = "Distance", value = f"{round(calcular_distancia(df_puntos['lat'][0], df_puntos['lon'][0], getLoc_calle.latitude, getLoc_calle.longitude),2)} m")
