0e17728924735003ce14eb67ca3792278ce243ec
albamrtnz <94964555+albamrtnz@users.noreply.github.com>
2023-06-26 17:23:26 +0200

    proyEDM.py
    comments of the code

--- proyEDM.py
+++ proyEDM.py
@@ -22,7 +22,7 @@ data = {'lat': [], 'lon': []}
 
 st.title("Nearest Bike Parking Finder")
 
-#to auto-update components
+### to auto-update components
 placeholder = st.empty()
 
 def obtener_tiempo_madrid():
@@ -64,20 +64,20 @@ with placeholder.container():
     m3.metric(label= "Conditions", value="{}".format(tiempo_madrid['current']['condition']['text']), delta = "{}".format(tiempo_anterior['forecast']['forecastday'][0]['day']['condition']['text']), delta_color= 'off')
 
     time.sleep(1)
-
+###
 
 coord_geo = {"Madrid" : {"lon": -3.7025600, "lat": 40.4165000}}
 
 col1,col2 = st.columns(2)
 with col1:
     ciudad = "Madrid"
-    destino = st.text_input("Origin", "Ejemplo: Gran Vía")
+    destino = st.text_input("Origin", "Example: Gran Vía")
 
-with col2:
-    # Insertar un espacio en blanco para poner "elegir tema" abajo del todo
+with col2:                                                #Column for the map
     df_puntos = {'lat':[], 'lon':[]}
     lista_bicis = pd.read_csv("bicis1.csv", header = 0, sep = ';', encoding='latin-1')
-    distrito = []
+    distrito = {}
+    cont = 0
     for dis in lista_bicis['Distrito']:
         dis_final = dis[4:]
         if dis_final == 'CHAMBERê':
@@ -86,11 +86,11 @@ with col2:
             dis_final = 'TETUÁN'
         if dis_final == 'CHAMARTêN':
             dis_final = 'CHAMARTÍN'
-        distrito.append(dis_final)
+        distrito[cont] = dis_final
         
         
-    if destino != "Eje: Gran Vía":
-        def dirToCoord(df, calle):
+    if destino != "Example: Gran Vía":
+        def dirToCoord(df, calle):                        #With dirToCoord() we obtain the nearest bike parking from the location provided
             minimo = 10000000
             for i in range(len(df)):
                 eje_lon = abs(float(df.loc[i,'Longitud'])-calle.longitude)
@@ -101,17 +101,17 @@ with col2:
                     fila = i
             return fila
         
-        if "Madrid, España" not in destino:
-            u = destino + ", Madrid, España"
-            getLoc_calle = loc.geocode(u)
+        if "Madrid, España" not in destino:                #Adding Madrid as prederterminated location 
+            u = destino + ", Madrid, España" 
+            getLoc_calle = loc.geocode(u)                  #Getting coords of the place provided by the user
             if getLoc_calle:
                 row = dirToCoord(lista_bicis, getLoc_calle)
                 df_puntos['lon'].append(float(lista_bicis.loc[row, 'Longitud']))
-                df_puntos['lat'].append(float(lista_bicis.loc[row, 'Latitud']))
-                                                                                 
+                df_puntos['lat'].append(float(lista_bicis.loc[row, 'Latitud']))                                                                    
             else:
                 pass
-        if len(df_puntos['lat']) > 0:
+                
+        if len(df_puntos['lat']) > 0:                     #If we have obtained a point representing the nearest bike parking
             fig = go.Figure(go.Scattermapbox(
                 mode='markers+text',
                 name = 'PARKING',
@@ -135,21 +135,21 @@ with col2:
             fig.update_layout(height=800,width=1000) 
             u = lista_bicis.loc[row, 'Calle'] +", Madrid, España"
             
-        else: 
+        else:                                             #Else, we have not obtained a point representing the nearest bike parking, so the user just see the map of Madrid.
             fig = px.scatter_mapbox(df_puntos, lat='lat', lon='lon', center = coord_geo['Madrid'], zoom = 11)
             fig.update_layout(mapbox_style="carto-positron")
             fig.update_layout(height=800,width=1000)
-    
-    
-    # Mostrar el mapa interactivo en Streamlit
+
+    #Showing the map
     st.plotly_chart(fig)
 
-with col1:
+with col1:                                                #Column for the extra information
     if loc.geocode(u):
         st.write(f'{loc.geocode(u)}')
         if len(df_puntos['lat']) > 0:
-            with st.expander('Graphics'):
-                st.write("The chart above shows the distribution plot of the available slots in the bike's parkings")
+            with st.expander('Graphics'):                 #In the expander we will plot the distribution plot of the available slots
+                st.write("The chart below shows the distribution plot of the available slots in the bike's parkings")
+                # Mostrar el gráfico de distribución
                 distribution_plot(lista_bicis.iloc[row, 9])
     else: pass
         
