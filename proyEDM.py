ad0dd69def55b4f91dd4385c4c822455f7d3ef6b
albamrtnz <94964555+albamrtnz@users.noreply.github.com>
2023-06-26 19:03:26 +0200

    Update proyEDM.py

--- proyEDM.py
+++ proyEDM.py
@@ -151,12 +151,10 @@ with col1:                                                #Column for the extra 
         st.write(f'Origin: {loc.geocode(u)}')
         u2 = f'{lista_bicis.loc[row, "Calle"]}, Madrid, España'
         st.write(f'Bike parking: {loc.geocode(u2)}')
-        placeholder2 = st.empty()
-        with placeholder2.container():
-            w1,w2 = st.columns(2)
-            w1.write(f'Capacity: {lista_bicis.iloc[row, 9]} bikes')
-            w2.metric(label = "Distance", value = f"{round(calcular_distancia(df_puntos['lat'][0], df_puntos['lon'][0], getLoc_calle.latitude, getLoc_calle.longitude),2)} m")
-            
+        st.write('--')
+        st.write(f'Capacity: {lista_bicis.iloc[row, 9]} bikes')
+        st.metric(label = "Distance", value = f"{round(calcular_distancia(df_puntos['lat'][0], df_puntos['lon'][0], getLoc_calle.latitude, getLoc_calle.longitude),2)} m")
+        
         if len(df_puntos['lat']) > 0:
             with st.expander('Graphics'):                 #In the expander we will plot the distribution plot of the available slots
                 st.write("The chart below shows the distribution plot of the available slots in the bike's parkings")
