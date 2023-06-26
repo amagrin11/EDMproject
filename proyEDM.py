b7a32d02b334d7c6dd8b6dbfe4869080479262bd
albamrtnz <94964555+albamrtnz@users.noreply.github.com>
2023-06-26 17:31:08 +0200

    distance proyEDM.py

--- proyEDM.py
+++ proyEDM.py
@@ -13,6 +13,8 @@ from time import sleep
 import requests
 from datetime import datetime, timedelta
 from plot_distrib import *
+from distance_coords import *
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 loc = Nominatim(user_agent="GetLoc")
@@ -144,6 +146,7 @@ with col2:                                                #Column for the map
 with col1:                                                #Column for the extra information
     if loc.geocode(u):
         st.write(f'{loc.geocode(u)}')
+        st.write(calcular_distancia(df_puntos['lat'][0], df_puntos['lon'][0], df_puntos['lat'][1], df_puntos['lon'][1]))
         if len(df_puntos['lat']) > 0:
             with st.expander('Graphics'):                 #In the expander we will plot the distribution plot of the available slots
                 st.write("The chart below shows the distribution plot of the available slots in the bike's parkings")
