eb70eb0b92c613925d6338cb4988a674062a66ce
amagrin11 <96311377+amagrin11@users.noreply.github.com>
2023-06-26 19:35:00 +0200

    Update proyEDM.py

--- proyEDM.py
+++ proyEDM.py
@@ -101,7 +101,7 @@ with col2:                                                #Column for the map
             return fila
         
         if "Madrid, España" not in destino:                #Adding Madrid as prederterminated location 
-            u = destino.upper() + ", Madrid, España" 
+            u = destino[0].upper()+ destino[1:] + ", Madrid, España" 
             getLoc_calle = loc.geocode(u)                  #Getting coords of the place provided by the user
             
             if getLoc_calle:                              
