dec4832092eb5e68f57a96c24aa5b8ffce843595
albamrtnz <94964555+albamrtnz@users.noreply.github.com>
2023-06-26 17:59:36 +0200

    Update proyEDM.py

--- proyEDM.py
+++ proyEDM.py
@@ -146,7 +146,7 @@ with col2:                                                #Column for the map
 with col1:                                                #Column for the extra information
     if loc.geocode(u):
         st.write(f'{loc.geocode(u)}')
-        st.write(calcular_distancia(df_puntos['lat][0], df_puntos['lon'][0], getLoc_calle.latitude, getLoc_calle.longitude))
+        st.write(calcular_distancia(df_puntos['lat'][0], df_puntos['lon'][0], getLoc_calle.latitude, getLoc_calle.longitude))
         
         if len(df_puntos['lat']) > 0:
             with st.expander('Graphics'):                 #In the expander we will plot the distribution plot of the available slots
